b790d765db784cde57a1b4f89e5e617fd611ec42
OneFourteen Studio <onefourteenstudio114@gmail.com>
2022-10-15 11:44:36 -0400

    product page

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
+// const button = document.querySelector(".product-button")
+// const size = document.querySelector("#Size")
+// const scent = document.querySelector("#Scent")
 
+// size.addEventListener("change", () => {
+//   button.setAttribute("data-item-custom1-value", size.value)
+// })
 
+// scent.addEventListener("change", () => {
+//   button.setAttribute("data-item-custom2-value", scent.value)
+// })

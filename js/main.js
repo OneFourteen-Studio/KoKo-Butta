e8d831d60e469b851bb1389acd15c79647fce1df
OneFourteen Studio <onefourteenstudio114@gmail.com>
2022-10-15 14:19:34 -0400

    product price change

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
-// const button = document.querySelector(".product-button")
-// const size = document.querySelector("#Size")
-// const scent = document.querySelector("#Scent")
+// product price change
+const button = document.querySelector(".product-button")
+const size = document.querySelector("#Size")
+const scent = document.querySelector("#Scent")
+let selectedOption = size.options[size.selectedIndex].value
+console.log(selectedOption)
+const newPrice = document.querySelector(".item-price")
+console.log(newPrice.textContent)
 
-// size.addEventListener("change", () => {
-//   button.setAttribute("data-item-custom1-value", size.value)
-// })
+size.addEventListener("change", () => {
+  button.setAttribute("data-item-custom1-value", size.value)
+  let selectedOption = size.options[size.selectedIndex].value
+  const newPrice = document.querySelector(".item-price")
+  if (selectedOption === "Large") {
+    newPrice.textContent = "$12.50"
+  } else {
+    newPrice.textContent = "$10"
+  }
+})
 
-// scent.addEventListener("change", () => {
-//   button.setAttribute("data-item-custom2-value", scent.value)
-// })
+scent.addEventListener("change", () => {
+  button.setAttribute("data-item-custom2-value", scent.value)
+})
+
+
+// product image change
+

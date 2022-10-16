2f8d5acf443435facd58526cb7fd92e0f94579eb
OneFourteen Studio <onefourteenstudio114@gmail.com>
2022-10-16 10:26:23 -0400

    finshed product page layout and scripts

--- js/main.js
+++ js/main.js
@@ -3,9 +3,7 @@ const button = document.querySelector(".product-button")
 const size = document.querySelector("#Size")
 const scent = document.querySelector("#Scent")
 let selectedOption = size.options[size.selectedIndex].value
-console.log(selectedOption)
 const newPrice = document.querySelector(".item-price")
-console.log(newPrice.textContent)
 
 size.addEventListener("change", () => {
   button.setAttribute("data-item-custom1-value", size.value)
@@ -18,10 +16,29 @@ size.addEventListener("change", () => {
   }
 })
 
+// product image change
 scent.addEventListener("change", () => {
   button.setAttribute("data-item-custom2-value", scent.value)
+  let selectedMainImage = document.querySelector("#main-image")
+  let selectedImages = document.querySelectorAll(".product-images")
+  let selectedScent = scent.options[scent.selectedIndex].value
+  if(selectedScent === "Tropikal Paradise") {
+    selectedMainImage.src = selectedImages[1].src
+  } else if (selectedScent === "Sweet Kandy") {
+    selectedMainImage.src = selectedImages[2].src
+  } else if (selectedScent === "Mango Madness") {
+    selectedMainImage.src = selectedImages[3].src
+  }
 })
 
+// image change on click
+let mainImage = document.getElementById("main-image")
+let images = document.querySelectorAll(".product-images")
 
-// product image change
+images.forEach((image) => {
+  image.addEventListener("click", (event) => {
+    mainImage.setAttribute("src", image.getAttribute("src"))
+    console.log(mainImage)
+  })
+})
 
